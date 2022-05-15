fd2601ca89443f0b5855c51ab8a70ae72d90dc85
Danylo Nikora <d.nikora2019@gmail.com>
2022-05-15 18:03:40 +0300

    fix formatCurrency function

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,10 +1,13 @@
+import { NON_BREAKING_SPACE as NBS } from "../constants/nonBreakingSpace";
+
 function formatCurrency(num) {
   return (
     Intl.NumberFormat("uk-UA", {
       style: "decimal",
       minimumFractionDigits: 2,
-    }).format(num) + " грн"
+      maximumFractionDigits: 2,
+    }).format(num) + NBS + "грн"
   );
-};
+}
 
 export default formatCurrency;
