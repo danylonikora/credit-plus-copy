678de2552f574bcb10bec9a00e1bbe63364190c2
Danylo Nikora <d.nikora2019@gmail.com>
2022-05-21 14:18:16 +0300

    add shared html head for pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,30 @@ import React from "react";
 import "../styles/global.scss";
 import Layout from "../components/layout/Layout/Layout";
 import TranslationContext from "../utils/TranslationContext";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
-    <TranslationContext.Provider value={pageProps.translation}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </TranslationContext.Provider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Credit Plus Copy</title>
+        <link
+          rel="icon"
+          href="/favicons/icon.svg"
+          type="image/svg+xml"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/favicons/apple-touch-icon.png"
+        />
+      </Head>
+      <TranslationContext.Provider value={pageProps.translation}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </TranslationContext.Provider>
+    </>
   );
 }
 
